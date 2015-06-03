11ebb28003e897e0c32c2ccdbf50f301fafb58e4
Josua Schmid <josua.schmid@renuo.ch>
2015-06-03 11:42:31 +0200

    adjusted GUI to match Spree 3 admin interface

--- app/overrides/spree/admin/spree_microcms/add_to_admin_menu.rb
+++ app/overrides/spree/admin/spree_microcms/add_to_admin_menu.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_menu',
+Deface::Override.new(virtual_path: 'spree/layouts/admin',
                      name: 'add_spree_microcms_to_admin_menu',
-                     insert_bottom: "[data-hook='admin_tabs']",
-                     partial: 'spree_microcms/admin/menu')
+                     insert_bottom: '[data-hook="admin_tabs"]',
+                     partial: 'spree_microcms/admin/main_menu_item')
